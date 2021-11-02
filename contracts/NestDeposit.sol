6b1d4d7b3a5c7ef7c341ed8626e3083d68383224
victoriawins <victoria_hk@icloud.com>
2021-11-02 21:51:04 +0800

    Nest deposit and withdraw

--- contracts/NestDeposit.sol
+++ contracts/NestDeposit.sol
@@ -5,6 +5,8 @@ interface Erc20 {
     function approve(address, uint256) external returns (bool);
 
     function transfer(address, uint256) external returns (bool);
+
+    function transferFrom(address, address, uint256) external returns (bool);
 }
 
 
@@ -18,6 +20,8 @@ interface CErc20 {
     function redeem(uint) external returns (uint);
 
     function redeemUnderlying(uint) external returns (uint);
+
+    function transfer(address, uint256) external returns (bool);
 }
 
 
@@ -33,16 +37,26 @@ interface CEth {
     function redeemUnderlying(uint) external returns (uint);
 }
 
-contract NestDeposit {
+interface IRecursive {
+    function checkReserve(
+        address _erc20Contract,
+        address _cErc20Contract) external returns (bool);
 
-    struct Deposit {
-        uint256 amount;
-        uint256 rate;
-        uint256 blockNumber;
+    function fillReserve(
+            address _erc20Contract,
+            address _cErc20Contract,
+            uint256 amount) external returns (bool)
+}
+
+contract NestDeposit is IRecursive {
+
+    struct Pool {
+        uint256 balance;
+        uint256 reserve;
     }
 
     struct Token {
-        Deposit[] deposits;
+        uint256 balance;
     }
 
     struct Account {
@@ -53,157 +67,128 @@ contract NestDeposit {
 
     event MyLog(string, uint256);
 
-    event DepositMade(address pool, address from, uint256 amount);
+    event DepositMade(address pool, address cerc20, address from, uint256 amount);
 
-    event WithdrawalMade(address pool, address to, uint256 amount);
+    event WithdrawalMade(address pool, address cerc20, address to, uint256 amount);
 
-    mapping(address => uint256) internal poolBalance;
+    mapping(address => Pool) internal pools;
+
+    function checkReserve(
+        address _erc20Contract,
+        address _cErc20Contract) external returns (bool) {
+
+        // Create a reference to the corresponding cToken contract, like cDAI
+        CErc20 cToken = CErc20(_cErc20Contract);
+
+        // Update pool balance
+        Pool storage pool = pools[_erc20Contract];
+        if(pool.balance > pool.reserve ) {
+            cToken.mint(( pool.balance - pool.reserve));
+        }
+
+        return true;
+    }
+
+       function fillReserve(
+            address _erc20Contract,
+            address _cErc20Contract,
+            uint256 amount) external returns (bool) {
+
+        // Create a reference to the corresponding cToken contract, like cDAI
+        CErc20 cToken = CErc20(_cErc20Contract);
+
+        // Redeem underlying
+        uint256 redeemed = cToken.redeemUnderlying(amount);
+
+        // Update pool balance
+        pools[_erc20Contract].balance += redeemed;
+
+        return true;
+    }
 
     function depositErc20(
         address _erc20Contract,
         address _cErc20Contract,
         uint256 _numTokensToSupply
-    ) public returns (bool) {
+    ) public returns (uint256) {        
         // Create a reference to the underlying asset contract, like DAI.
         Erc20 underlying = Erc20(_erc20Contract);
 
         // Create a reference to the corresponding cToken contract, like cDAI
         CErc20 cToken = CErc20(_cErc20Contract);
 
-        // Amount of current exchange rate from cToken to underlying
-        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit MyLog("Exchange Rate (scaled up): ", exchangeRateMantissa);
-
-        // Amount added to your supply balance this block
-        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit MyLog("Supply Rate: (scaled up)", supplyRateMantissa);
-
         // Approve transfer on the ERC20 contract
-        underlying.approve(address(this), _numTokensToSupply);
+        underlying.approve(_cErc20Contract, _numTokensToSupply);
 
-        // Collect tokens
-        bool transferred = underlying.transfer(address(this), _numTokensToSupply);
-
+        //  Record tokens
         // Get user account
         Account storage account = accounts[msg.sender];
 
         // Save deposit
         Token storage token = account.tokens[_erc20Contract];
 
-        // Update pool balance
-        poolBalance[_erc20Contract] += _numTokensToSupply;
-
-        token.deposits.push(Deposit(_numTokensToSupply, exchangeRateMantissa, block.number));
-
-        emit DepositMade(_erc20Contract, msg.sender,  _numTokensToSupply);
-
-        return transferred;
-    }
-
-    function supplyEthToCompound(address payable _cEtherContract)
-        public
-        payable
-        returns (bool)
-    {
-        // Create a reference to the corresponding cToken contract
-        CEth cToken = CEth(_cEtherContract);
-
-        // Amount of current exchange rate from cToken to underlying
+        // Get exchange rate
         uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit MyLog("Exchange Rate (scaled up by 1e18): ", exchangeRateMantissa);
 
-        // Amount added to you supply balance this block
-        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit MyLog("Supply Rate: (scaled up by 1e18)", supplyRateMantissa);
+        // Calculate cToken value
+        uint256 mintResult = _numTokensToSupply*exchangeRateMantissa;
 
-        cToken.mint{value: msg.value,gas: 250000}();
-        return true;
+        // Update pool balance
+        pools[_erc20Contract].balance += _numTokensToSupply;
+
+        // Give this contract the ERC20 Tokens
+        underlying.transfer(address(this), _numTokensToSupply);
+
+        // Calculate current balance  and add new balance to it
+        token.balance += mintResult;
+
+        // Check if we should mint
+        IRecursive(address(this)).checkReserve(_erc20Contract, _cErc20Contract);
+
+        return mintResult;
     }
 
-    function supplyErc20ToCompound(
+
+    function withdrawErc20Tokens(
+        uint256 amount,
         address _erc20Contract,
-        address _cErc20Contract,
-        uint256 _numTokensToSupply
-    ) internal returns (uint) {
+        address _cErc20Contract
+    ) internal returns (bool) {
         // Create a reference to the underlying asset contract, like DAI.
         Erc20 underlying = Erc20(_erc20Contract);
 
         // Create a reference to the corresponding cToken contract, like cDAI
         CErc20 cToken = CErc20(_cErc20Contract);
 
-        // Amount of current exchange rate from cToken to underlying
+        // Get user account
+        Account storage account = accounts[msg.sender];
+
+        // Check if user has enough funds
+        Token storage token = account.tokens[_erc20Contract];
+
         uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
-        emit MyLog("Exchange Rate (scaled up): ", exchangeRateMantissa);
 
-        // Amount added to you supply balance this block
-        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
-        emit MyLog("Supply Rate: (scaled up)", supplyRateMantissa);
+        uint256 underlyingAmount = token.balance / exchangeRateMantissa;
 
-        // Approve transfer on the ERC20 contract
-        underlying.approve(_cErc20Contract, _numTokensToSupply);
+        assert((underlyingAmount) > amount);
 
-        // Mint cTokens
-        uint mintResult = cToken.mint(_numTokensToSupply);
-        return mintResult;
-    }
+        // Get pool
+        Pool storage pool = pools[_erc20Contract];
 
-    function redeemCErc20TokensFromCompount(
-        uint256 amount,
-        bool redeemType,
-        address _cErc20Contract
-    ) internal returns (bool) {
-        // Create a reference to the corresponding cToken contract, like cDAI
-        CErc20 cToken = CErc20(_cErc20Contract);
-
-        // `amount` is scaled up, see decimal table here:
-        // https://compound.finance/docs#protocol-math
-
-        uint256 redeemResult;
-
-        if (redeemType == true) {
-            // Retrieve your asset based on a cToken amount
-            redeemResult = cToken.redeem(amount);
-        } else {
-            // Retrieve your asset based on an amount of the asset
-            redeemResult = cToken.redeemUnderlying(amount);
+        if(pool.reserve < amount) {
+            // Redeem underlying
+            IRecursive(address(this)).fillReserve(_erc20Contract, _cErc20Contract, amount);
         }
 
-        // Error codes are listed here:
-        // https://compound.finance/developers/ctokens#ctoken-error-codes
-        emit MyLog("If this is not 0, there was an error", redeemResult);
+        underlying.transferFrom(address(this), msg.sender, amount);
+
+        // Subtract cTokens
+        token.balance -= amount*exchangeRateMantissa;
 
         return true;
     }
 
-    function redeemCEthFromCompound(
-        uint256 amount,
-        bool redeemType,
-        address _cEtherContract
-    ) internal returns (bool) {
-        // Create a reference to the corresponding cToken contract
-        CEth cToken = CEth(_cEtherContract);
-
-        // `amount` is scaled up by 1e18 to avoid decimals
-
-        uint256 redeemResult;
-
-        if (redeemType == true) {
-            // Retrieve your asset based on a cToken amount
-            redeemResult = cToken.redeem(amount);
-        } else {
-            // Retrieve your asset based on an amount of the asset
-            redeemResult = cToken.redeemUnderlying(amount);
-        }
-
-        // Error codes are listed here:
-        // https://compound.finance/docs/ctokens#ctoken-error-codes
-        emit MyLog("If this is not 0, there was an error", redeemResult);
-
-        return true;
-    }
-
-    // This is needed to receive ETH when calling `redeemCEth`
      receive() external payable {
             // React to receiving ether
-        }
+     }
 }
