081b2a92b4d246576d8bdda8fbfdc21e6e3c4d89
victoriawins <victoria_hk@icloud.com>
2021-11-15 20:46:25 +0800

    Use DAI address pairs

--- client/src/constants/addresses.js
+++ client/src/constants/addresses.js
@@ -1,6 +1,6 @@
 export const RinkebyAddresses = [
     {name: "BAT", address: "0xebf1a11532b93a529b5bc942b4baa98647913002", token: true, ercAddress: "0x2fa6a0728a63115e6fc1eb8496ea94e86b8cdf7b" },
-    {name: "DAI", address: "0x6d7f0754ffeb405d23c51ce938289d4835be3b14", token: true, ercAddress: "0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658" },
+    {name: "DAI", address: "0x6D7F0754FFeb405d23C51CE938289d4835bE3b14", token: true, ercAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa" },
     {name: "REP", address: "0xebe09eb3411d18f4ff8d859e096c533cac5c6b60", token: true, ercAddress: "0x9cac8508b9ff26501439590a24893d80e7e84d21" },
     {name: "USDC", address: "0x5b281a6dda0b271e91ae35de655ad301c976edb1", token: true, ercAddress: "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b" },
     {name: "USDT", address: "0x2fb298bdbef468638ad6653ff8376575ea41e768", token: true, ercAddress: "0x3b00ef435fa4fcff5c209a37d1f3dcff37c705ad" },
